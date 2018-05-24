09fcd95f3cb0ba1c5c630b902f80595fc19a8459
Yurii Holiuk <yurii.holiuk@itrexgroup.com>
2018-05-24 14:45:41 +0300

    add max validator to date

--- src/react-test-task-app/react-test-task-app.js
+++ src/react-test-task-app/react-test-task-app.js
@@ -38,7 +38,7 @@ class ReactTestTaskApp extends PolymerElement {
         <form id="form" slot="form">
           <form-field name="firstName"required disallowed="[[disallowed]]" label="First name"></form-field>
           <form-field name="lastName" required disallowed="[[disallowed]]" label="Last name"></form-field>
-          <form-field name="birthday" type="date" required label="Birthday" always-active-label></form-field>
+          <form-field name="birthday" type="date" required max="[[today]]" label="Birthday" always-active-label></form-field>
           
           <radio-group name="sex" required label="Sex">
             <radio-button value="male" label="Male"></radio-button>
@@ -62,6 +62,7 @@ class ReactTestTaskApp extends PolymerElement {
 
     this.emailRegEx = '([a-z0-9][-a-z0-9_\\+\\.]*[a-z0-9])@([a-z0-9][-a-z0-9\\.]*[a-z0-9]\\.(arpa|root|aero|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|ee|eg|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)|([0-9]{1,3}\\.{3}[0-9]{1,3}))';
     this.disallowed = ['\'', '\"'];
+    this.today = new Date().toISOString().split('T')[0]; // TODO: make it better
   }
 
   onSubmit(event) {
